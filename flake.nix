40213232e8859ebd4a6b5704d48f71ed60adc52a
Andy Baizon <dev@baizon.uk>
2025-01-07 09:51:22 +0000

    feat: flake setup

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A Python Maze Solver";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      buildScript = pkgs.writeScriptBin "build" ''
+        #!${pkgs.bash}/bin/bash
+        PYTHONPATH=$PYTHONPATH:. ${pkgs.python312Full}/bin/python3 -m src.main
+      '';
+    in 
+  {
+    # pakages.default = mainPackage;
+    devShells.default = pkgs.mkShell {
+      packages = [ pkgs.zsh ];
+      buildInputs = [
+        pkgs.python312Full
+        pkgs.black
+        pkgs.pyright
+        buildScript
+      ];
+
+      shellHook = ''
+        export SHELL=${pkgs.zsh}/bin/zsh
+      
+        if [[ $SHELL != ${pkgs.zsh}/bin/zsh ]]; then
+          exec ${pkgs.zsh}/bin/zsh
+        fi
+      '';
+    };
+  });
+}
