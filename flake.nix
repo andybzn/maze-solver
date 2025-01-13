d86eea99f4721f2a0efe7095d7f43375e37c7cf3
Andy Baizon <dev@baizon.uk>
2025-01-13 12:36:02 +0000

    chore(flake): cleanup

--- flake.nix
+++ flake.nix
@@ -23,9 +23,33 @@
         '';
 
         testScript = pkgs.writeScriptBin "tests" ''
-          	      #!${pkgs.bash}/bin/bash
-          	      PYTHONPATH=$PYTHONPATH. ${pkgs.python312Full}/bin/python3 -m unittest discover -s tests
+          #!${pkgs.bash}/bin/bash
+          PYTHONPATH=$PYTHONPATH. ${pkgs.python312Full}/bin/python3 -m unittest discover -s tests
         '';
+
+        formatScript = pkgs.writeScriptBin "format" ''
+          #!${pkgs.bash}/bin/bash
+
+          show_usage() {
+            echo "Format Python code using Black"
+            echo "Usage:"
+            echo "  * format check - Check file formatting"
+            echo "  * format fix - Format all files in project"
+          }
+
+          case "$1" in
+            "check")
+              ${pkgs.black}/bin/black --check .
+              ;;
+            "fix")
+              ${pkgs.black}/bin/black .
+              ;;
+            *)
+              show_usage
+              ;;
+          esac
+        '';
+
       in
       {
         packages.default = buildScript;
@@ -37,10 +61,18 @@
             pkgs.basedpyright
             buildScript
             testScript
+            formatScript
           ];
 
           shellHook = ''
             export SHELL=${pkgs.zsh}/bin/zsh
+
+            echo "Development environment loaded!"
+            echo "Available commands:"
+            echo "  * build - Run the maze solver"
+            echo "  * tests - Execute unit tests"
+            echo "  * format check - Check file formatting"
+            echo "  * format fix - Format files in project"
 
             if [[ $SHELL != ${pkgs.zsh}/bin/zsh ]]; then
               exec ${pkgs.zsh}/bin/zsh
