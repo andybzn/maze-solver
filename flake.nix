23d4ab766a367f599e3b2e5c0138ddae4cb6eb47
Andy Baizon <dev@baizon.uk>
2025-01-13 10:31:03 +0000

    feat: support nix run

--- flake.nix
+++ flake.nix
@@ -5,41 +5,48 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
 
-      buildScript = pkgs.writeScriptBin "build" ''
-        #!${pkgs.bash}/bin/bash
-        PYTHONPATH=$PYTHONPATH:. ${pkgs.python312Full}/bin/python3 -m src.main
-      '';
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
 
-      testScript = pkgs.writeScriptBin "tests" ''
-	#!${pkgs.bash}/bin/bash
-	PYTHONPATH=$PYTHONPATH. ${pkgs.python312Full}/bin/python3 -m unittest discover -s tests
-      '';
-    in 
-  {
-    # pakages.default = mainPackage;
-    devShells.default = pkgs.mkShell {
-      packages = [ pkgs.zsh ];
-      buildInputs = [
-        pkgs.python312Full
-        pkgs.black
-        pkgs.basedpyright
-        buildScript
-	testScript
-      ];
+        buildScript = pkgs.writeScriptBin "build" ''
+          #!${pkgs.bash}/bin/bash
+          PYTHONPATH=$PYTHONPATH:. ${pkgs.python312Full}/bin/python3 -m src.main
+        '';
 
-      shellHook = ''
-        export SHELL=${pkgs.zsh}/bin/zsh
-      
-        if [[ $SHELL != ${pkgs.zsh}/bin/zsh ]]; then
-          exec ${pkgs.zsh}/bin/zsh
-        fi
-      '';
-    };
-  });
+        testScript = pkgs.writeScriptBin "tests" ''
+          	      #!${pkgs.bash}/bin/bash
+          	      PYTHONPATH=$PYTHONPATH. ${pkgs.python312Full}/bin/python3 -m unittest discover -s tests
+        '';
+      in
+      {
+        packages.default = buildScript;
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.zsh ];
+          buildInputs = [
+            pkgs.python312Full
+            pkgs.black
+            pkgs.basedpyright
+            buildScript
+            testScript
+          ];
+
+          shellHook = ''
+            export SHELL=${pkgs.zsh}/bin/zsh
+
+            if [[ $SHELL != ${pkgs.zsh}/bin/zsh ]]; then
+              exec ${pkgs.zsh}/bin/zsh
+            fi
+          '';
+        };
+      }
+    );
 }
